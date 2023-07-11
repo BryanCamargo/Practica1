55a758ff9526843977a4ac04614441ed3bd93072
BryanCamargo <2021371063@uteq.edu.mx>
2023-07-10 22:49:19 -0600

    conexion a firebase

--- app/src/main/java/com/example/practica1/MainActivity.kt
+++ app/src/main/java/com/example/practica1/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.practica1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mAuth = FirebaseAuth.getInstance()
     }
-    
+
 }
